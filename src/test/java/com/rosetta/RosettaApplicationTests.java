9e3378728ef2d2539b38b5b6393c260b6f381e84
swy-326 <sungwon.yoon@usp.br>
2022-10-06 21:49:48 -0300

    initial files

---
+++ src/test/java/com/rosetta/RosettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
